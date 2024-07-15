4159e61c149b72201bad702ca4fafa4be413775b
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2024-07-15 19:06:39 +0300

    AGPL
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/spawn_util.rs
+++ src/spawn_util.rs
@@ -1,3 +1,22 @@
+/*
+    This file is a part of alloc-perf-test.
+
+    Copyright (C) 2024 Mohammad AlSaleh <CE.Mohammad.AlSaleh at gmail.com>
+    https://github.com/MoSal
+
+    alloc-perf-test is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Affero GNU General Public License for more details.
+
+    You should have received a copy of the Affero GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 use async_global_executor as a_exec;
 use std::future::Future;
 
